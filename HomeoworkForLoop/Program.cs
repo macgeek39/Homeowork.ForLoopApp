eaa6be5fa6efd4051218c090b0b66792cc0b6493
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-08 08:13:31 -0600

    Initialized commit

--- HomeoworkForLoop/Program.cs
+++ HomeoworkForLoop/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+/*
+ * Ask the user for a comma-separated list
+ * of first names (no spaces). Split the 
+ * string into a string array. 
+ * Loop through the array and print
+ * "Hello <name>" to the console for 
+ * each person.
+ */
+
+// Ask the user for a comm-separated list of first names
+// Split the string into a string array
+// loop through the array
+// print "Hello <name>" to the console for each person
